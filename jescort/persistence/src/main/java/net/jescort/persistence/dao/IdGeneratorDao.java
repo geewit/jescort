9609b91fc227c24b725874b35db6ef3c1177701a
sinosaga <sinosaga@gmail.com>
2012-02-22 13:36:11 +0800

    fix

--- jescort/persistence/src/main/java/net/jescort/persistence/dao/IdGeneratorDao.java
+++
@@ -1,9 +0,0 @@
-package net.jescort.persistence.dao;
-
-
-import net.jescort.domain.enums.IdName;
-
-public interface IdGeneratorDao
-{
-    public Integer newId(IdName idName);
-}
---
+++ jescort/web/src/main/java/net/jescort/web/servlet/controller/auth/MyAttachmentsController.java
@@ -0,0 +1,33 @@
+package net.jescort.web.servlet.controller.auth;
+
+import net.jescort.domain.user.User;
+import net.jescort.repository.EscortRepository;
+import org.apache.shiro.SecurityUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.annotation.Resource;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: admin@gelif.net
+ * Date: 12-2-8
+ * Time: 下午12:16
+ */
+@Controller
+public class MyAttachmentsController
+{
+    @RequestMapping(value = {"/auth/attachments/page/{pageNo}"}, method = RequestMethod.GET)
+    public ModelAndView attachmentsHandler(@PathVariable("pageNo") Integer pageNo)
+    {
+        User currentUser = (User) SecurityUtils.getSubject().getPrincipal();
+        ModelAndView mav = new ModelAndView("auth/attachments");
+        return escortRepository.attachmentView(currentUser.getId(), pageNo, 10, mav);
+    }
+
+    @Resource(name = "escortRepository")
+    private EscortRepository escortRepository;
+}
