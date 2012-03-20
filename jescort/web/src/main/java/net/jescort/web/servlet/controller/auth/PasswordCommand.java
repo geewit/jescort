dfe61cfc9b1ef8bb784f6064fbe82f1c37aa911d
sinosaga <sinosaga@gmail.com>
2012-03-20 08:31:42 +0800

    add change password in auth module

---
+++ jescort/web/src/main/java/net/jescort/web/servlet/controller/auth/PasswordCommand.java
@@ -0,0 +1,11 @@
+package net.jescort.web.servlet.controller.auth;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: admin@gelif.net
+ * Date: 12-3-19
+ * Time: 下午7:57
+ */
+public class PasswordCommand
+{
+}
---
+++ jescort/web/src/main/java/net/jescort/web/servlet/controller/auth/PasswordController.java
@@ -0,0 +1,11 @@
+package net.jescort.web.servlet.controller.auth;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: admin@gelif.net
+ * Date: 12-3-19
+ * Time: 下午6:02
+ */
+public class PasswordController
+{
+}
